7a5d43625fcc3220f162710d21c74ab0bf9a6dcd
David Wollschlegel <david@wollschlegel.com>
2020-12-07 20:36:10 +0100

    change port back to 8080

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const osc = require("osc"),
 // Create an Express server app
 // and serve up a directory of static files.
 const app = express()
-const port = 8082
+const port = 8080
 const server = app.listen(port,function(){
 	console.log('app running open socket at port 8080 listening for OSC at port 8002')
 })
